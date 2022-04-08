b2b0967af851274f109bb77a7c471967d3f3f8da
John Molakvoæ <skjnldsv@protonmail.com>
2022-04-08 16:15:29 +0200

    Allow share initiator to set Download limits
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -152,8 +152,8 @@ class ApiController extends OCSController {
 			throw new OCSNotFoundException('Unknown share');
 		}
 
-		// Make sure the user is owner of the share
-		if ($user == null || $share->getShareOwner() !== $user->getUID()) {
+		// Make sure the user is owner of the share or initiator
+		if ($user == null || ($share->getShareOwner() !== $user->getUID() && $share->getSharedBy() !== $user->getUID())) {
 			throw new OCSNotFoundException('Unknown share');
 		}
 
