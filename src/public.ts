23a4ccf24c28b50d92068ba5836ae465e1f6dc54
Christopher Ng <chrng8@gmail.com>
2024-02-21 18:52:18 -0800

    chore: Move to ts
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- src/public.ts
+++ src/public.ts
@@ -1,6 +1,7 @@
 /**
  * @copyright Copyright (c) 2021 John Molakvoæ <skjnldsv@protonmail.com>
  *
+ * @author Christopher Ng <chrng8@gmail.com>
  * @author John Molakvoæ <skjnldsv@protonmail.com>
  *
  * @license GNU AGPL version 3 or any later version
@@ -19,13 +20,14 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 import { loadState } from '@nextcloud/initial-state'
 import { translate as t, translatePlural as n } from '@nextcloud/l10n'
 
 import '../css/public.css'
 
-const { limit, downloads } = loadState(appName, 'download_limit', { limit: -1, downloads: 0 })
-console.debug('[DEBUG]', appName, { limit, downloads })
+const { limit, downloads } = loadState('files_downloadlimit', 'download_limit', { limit: -1, downloads: 0 })
+console.debug('[DEBUG]', 'files_downloadlimit', { limit, downloads })
 
 // Global variables init on page load
 let count = limit - downloads
@@ -34,57 +36,66 @@ let clicks = 0
 /**
  * Update the span counter message
  *
- * @param {Element} span the html dom element to edit
- * @param {number} count how much downloads are still allowed
+ * @param span html span element to update
+ * @param count number of remaining downloads allowed
  */
-const updateCounter = function(span, count) {
+const updateCounter = (span: HTMLSpanElement, count: number) => {
 	if (count === 0) {
-		span.innerText = t(appName, 'You have reached the maximum amount of downloads allowed')
+		span.innerText = t('files_downloadlimit', 'You have reached the maximum amount of downloads allowed')
 	} else {
-		span.innerText = n(appName, '1 remaining download allowed', '{count} remaining downloads allowed', count, { count })
+		span.innerText = n('files_downloadlimit', '1 remaining download allowed', '{count} remaining downloads allowed', count, { count })
 	}
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-	if (limit > 0) {
-		const container = document.getElementById('header-primary-action')
-		const span = document.createElement('span')
-
-		span.style = 'color: var(--color-primary-text); padding: 0 10px;'
-		updateCounter(span, count)
-
-		container.prepend(span)
-
-		// Preventing mouse interaction
-		document.querySelector('#files-public-content').oncontextmenu = function(event) {
-			event.preventDefault()
-			event.stopPropagation()
-			return false
-		}
-
-		// Adding double-download warning
-		const downloadButtons = document.querySelectorAll('a[href*="/download/"]') || []
-		new Set(downloadButtons).forEach(button => {
-			button.addEventListener('click', (event) => {
-				// Warn about download limits
-				if (clicks > 0) {
-					if (!confirm(t(appName, 'This share has a limited number of downloads. Are you sure you want to trigger a new download?'))) {
-						event.preventDefault()
-						event.stopPropagation()
-						return
-					}
-				}
-
-				// Handle counts changes
-				count--
-				clicks++
-				updateCounter(span, count)
-
-				// Remove the buttons if share is now expired
-				if (count === 0) {
-					[...downloadButtons].forEach(button => button.remove())
-				}
-			})
-		})
+window.addEventListener('DOMContentLoaded', () => {
+	if (limit < 1) {
+		return
 	}
+
+	const container = document.getElementById('header-primary-action')
+	if (!container) {
+		return
+	}
+
+	const span = document.createElement('span')
+	span.setAttribute('style', 'color: var(--color-primary-text); padding: 0 10px;')
+	updateCounter(span, count)
+	container.prepend(span)
+
+	const publicContent = document.querySelector<HTMLElement>('#files-public-content')
+	if (!publicContent) {
+		return
+	}
+
+	// Preventing mouse interaction
+	publicContent.oncontextmenu = (event) => {
+		event.preventDefault()
+		event.stopPropagation()
+		return false
+	}
+
+	// Adding double-download warning
+	const downloadButtons = document.querySelectorAll<HTMLAnchorElement>('a[href*="/download/"]') || []
+	new Set(downloadButtons).forEach(button => {
+		button.addEventListener('click', (event) => {
+			// Warn about download limits
+			if (clicks > 0) {
+				if (!confirm(t('files_downloadlimit', 'This share has a limited number of downloads. Are you sure you want to trigger a new download?'))) {
+					event.preventDefault()
+					event.stopPropagation()
+					return
+				}
+			}
+
+			// Handle counts changes
+			count--
+			clicks++
+			updateCounter(span, count)
+
+			// Remove the buttons if share is now expired
+			if (count === 0) {
+				[...downloadButtons].forEach(button => button.remove())
+			}
+		})
+	})
 })
--- webpack.js
+++ webpack.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-webpackConfig.entry.public = path.resolve(path.join('src', 'public.js'))
+webpackConfig.entry = {
+	public: path.resolve(path.join('src', 'public.ts'))
+}
 
 module.exports = webpackConfig
