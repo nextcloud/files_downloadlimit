514020b556b8a7baa644dc3b68eb17047249296b
Christopher Ng <chrng8@gmail.com>
2024-03-05 08:21:17 -0800

    feat: Register new download limit entry
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 /**
  * @copyright Copyright (c) 2021 John Molakvoæ <skjnldsv@protonmail.com>
  *
+ * @author Christopher Ng <chrng8@gmail.com>
  * @author John Molakvoæ <skjnldsv@protonmail.com>
  *
- * @license GNU AGPL version 3 or any later version
+ * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -20,55 +21,9 @@
  *
  */
 
-// Create dummy global store
-export default class Store {
+import { DownloadLimitAction } from './actions/DownloadLimitAction.ts'
 
-	_data = {
-		enabled: false,
-		limit: null,
-		count: null,
-		token: null,
-		loading: false,
-	}
-
-	get enabled() {
-		return this._data.enabled
-	}
-
-	set enabled(enabled) {
-		this._data.enabled = enabled
-	}
-
-	get limit() {
-		return this._data.limit
-	}
-
-	set limit(limit) {
-		this._data.limit = limit
-	}
-
-	get count() {
-		return this._data.count
-	}
-
-	set count(count) {
-		this._data.count = count
-	}
-
-	get token() {
-		return this._data.token
-	}
-
-	set token(token) {
-		this._data.token = token
-	}
-
-	get loading() {
-		return this._data.loading
-	}
-
-	set loading(loading) {
-		this._data.loading = loading
-	}
-
-}
+window.addEventListener('DOMContentLoaded', () => {
+	// @ts-expect-error OCA is not public
+	OCA.Sharing.ExternalShareActions.registerAction(new DownloadLimitAction())
+})
