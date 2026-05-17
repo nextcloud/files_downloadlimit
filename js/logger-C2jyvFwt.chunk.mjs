f51962fc0bb35ed4e8e27454fa50058768eb6cb9
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-05-17 04:13:08 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

---
+++ js/logger-C2jyvFwt.chunk.mjs
@@ -0,0 +1,2 @@
+import{c as o}from"./translation-DoG5ZELJ-DCcmw7nq.chunk.mjs";const i=o().setApp("files_downloadlimit").detectUser().build();export{i as l};
+//# sourceMappingURL=logger-C2jyvFwt.chunk.mjs.map
--- js/logger-DRUu-xbg.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{g as o}from"./index-BCrAQR8N.chunk.mjs";const i=o().setApp("files_downloadlimit").detectUser().build();export{i as l};
-//# sourceMappingURL=logger-DRUu-xbg.chunk.mjs.map
